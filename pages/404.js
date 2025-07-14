c20a53ffbcfec7825cb56199bed2fd2ff6d2dcf7
Hossein Khedmati <khedmati.hossein1382@gmail.com>
2025-07-14 16:54:07 +0330

    404 and profile mobile design added to project successfully

--- pages/404.js
+++ pages/404.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import styles from "../styles/404.module.css";
 import Image from "next/image";
+import Link from "next/link";
 
 function NOTfound() {
   return (
@@ -11,7 +12,7 @@ function NOTfound() {
       <div className={styles.container}>
         <div className={styles.texts}>
           <h1>صفحه مورد نظر یافت نشد!</h1>
-          <button>بازگشت به صفحه اصلی</button>
+          <Link href="/"><button>بازگشت به صفحه اصلی</button></Link>
         </div>
         <div className={styles.image}>
           <Image priority alt="404.png" src="/images/Error-TV.png" width={555} height={555}/>
